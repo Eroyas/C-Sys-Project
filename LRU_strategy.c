1169a3359fc4f6abf800861d18ad2ed54296fe35
Yasin Eroglu <ey403505@etu.unice.fr>
2015-06-09 14:23:55 +0200

    Fin de LRU_strategy

--- LRU_strategy.c
+++ LRU_strategy.c
@@ -12,87 +12,72 @@
 #include "low_cache.h"
 #include "cache_list.h"
 
-/* Transforme le pointeur sur la stratégie en un pointeur sur la liste LRU */
-#define LRU_LIST(pcache) ((struct Cache_List *)((pcache)->pstrategy))
-
-/* ------------------------------------------------------------------
- * Fonctions externes (utlisées par cache.c)
- * ------------------------------------------------------------------
- */
-
-/* Initialisation de la stratégie 
- * ------------------------------ 
- * On alloue et initialise la liste LRU.
-*/
-void *Strategy_Create(struct Cache *pcache) 
-{
-    /* Création et initialisation de la liste LRU */
+// Initialisation de la stratégie LRU
+void *Strategy_Create(struct Cache *pcache) {
+    // Allocation et initialisation de la liste LRU
     return Cache_List_Create();
 }
 
-/* Fermeture de la stratégie
- * -------------------------
- */
-void Strategy_Close(struct Cache *pcache)
-{
-    Cache_List_Delete(LRU_LIST(pcache));
+// Suppression de la liste LRU
+void Strategy_Close(struct Cache *pcache) {
+    Cache_List_Delete(pcache->pstrategy);
 }
 
-/* Invalidation du cache
- * ---------------------
- * On vide la liste LRU.
- */
-void Strategy_Invalidate(struct Cache *pcache)
-{
-    Cache_List_Clear(LRU_LIST(pcache));
+// Vide la liste LRU
+void Strategy_Invalidate(struct Cache *pcache) {
+    Cache_List_Clear((struct Cache_List*) pcache->pstrategy);
 }
 
-/* Algorithme de remplacement de bloc
- * ---------------------------------- 
-
- * On prend le premier bloc invalide. S'il n'y en a plus on prend le premier
- * bloc de la liste LRU c'est-à-dire le bloc le plus ancien. Comme ce bloc
- * devient le plus récent, on le passe en fin de liste.
+/* 
+ * On remplace ici le bloc qui a été le moins récemment accédé.
+ * Chaque fois qu’un bloc change d’affectation, on le transfère en queue
+ * de liste. Mais on effectue également ce transfert vers la queue chaque 
+ * fois qu’un bloc est utilisé, c’est-à-dire chaque fois qu’il est atteint 
+ * par une opération de lecture ou d’écriture. Ainsi le bloc en tête de la 
+ * liste lru est le bloc le moins récemment utilisé, et c’est lui qui 
+ * sera remplacé.
+ * 
+ * On prend donc le premier bloc de la liste LRU, le bloc le plus ancien,
+ * et on le passe en fin de liste car il devient du coup le plus récent. 
  */
-struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
-{
+struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) {
+    
+    // On ajoute au cache une structure de données supplémentaire, 
+    // une liste de pointeurs sur les blocs valides du cache.
+
     struct Cache_Block_Header *pbh;
-    struct Cache_List *lru_list = LRU_LIST(pcache);
+    struct Cache_List *lru_list = (struct Cache_List*) pcache->pstrategy;
 
     /* On cherche d'abord un bloc invalide */
-    if ((pbh = Get_Free_Block(pcache)) != NULL)
-    {
-        /* Les blocs invalides ne sont pas dans la liste ; mettons-le en queue */
+    if ((pbh = Get_Free_Block(pcache)) != NULL) {
+        // On ajoute les blocs invalides dans la liste.
         Cache_List_Append(lru_list, pbh);  
         return pbh;
     }
 
-    /* Sinon on prend le premier bloc de la liste LRU et on le déplace à la fin
-     * de la liste */
+    // Sinon on prend le premier bloc et le passe en fin de liste.
     pbh = Cache_List_Remove_First(lru_list);
     Cache_List_Append(lru_list, pbh);
 
     return pbh;    
 }
 
-/* Fonctions "réflexes" en cas de lecture et d'écriture
- * ---------------------------------------------------- 
- * Le bloc référencé devient le bloc le plus récent et passe donc en queue de
- * la liste LRU.
- */
-void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
-{
-    Cache_List_Move_To_End(LRU_LIST(pcache), pbh);
+ /*!
+ * LRU : La fréquence des accès compte, le bloc référencé devient le bloc le 
+ * plus recent et passe en fin de liste.
+ */  
+void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) {
+    Cache_List_Move_To_End((struct Cache_List*) pcache->pstrategy, pbh);
 }  
-  
-void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
-{
-    Cache_List_Move_To_End(LRU_LIST(pcache), pbh);
+
+ /*!
+ * LRU : La fréquence des accès compte, le bloc référencé devient le bloc le 
+ * plus recent et passe en fin de liste.
+ */ 
+void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh) {
+    Cache_List_Move_To_End((struct Cache_List*) pcache->pstrategy, pbh);
 } 
 
-/* Identification de la stratégie
- * ------------------------------
- */
 char *Strategy_Name()
 {
     return "LRU";
