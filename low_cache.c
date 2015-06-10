c99ea06394f4a6b5d517ba8a3329a15429035a69
Ugo <ugo.elkoubi@wanadoo.fr>
2015-06-10 15:03:29 +0200

    Low_cache peut etre fonctionnel

--- low_cache.c
+++ low_cache.c
@@ -6,7 +6,25 @@
  * 
  * \author : Yasin EROGLU - Jonathan BOUDAB - Ugo EL KOUBI - Théo DONZELLE
  *
- */
+*/
 
- #include "low_cache.h"
+#include "low_cache.h"
  
+/*Retourne le premier bloc libre du cache ou le pointeur NULL si le cache est plein. Cette
+fonction doit être invoquée par la stratégie de remplacement avant de considérer
+l’utilisation d’un bloc valide.*/
+struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache)
+{
+	unsigned int i;
+	struct Cache_Block_Header *headerCourant;
+	// on parcourt tous les blocs du cache
+	for(i = 0;i<pcache->nblocks;i++){
+		//on stocke le bloc courant
+		headerCourant = &(pcache->headers[i]);
+		// si il n'est pas valide 
+		if (headerCourant->flags & VALID != 0x1)
+			return headerCourant;
+	}
+	// le cache est plein
+	return NULL;
+}
